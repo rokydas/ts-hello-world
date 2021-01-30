a754bfc0f5fbadab1dbb41276386ca86bc905066
rokydas <droky136@gmail.com>
2021-01-30 10:48:45 +0600

    lesson 6 done

--- output/script.js
+++ output/script.js
@@ -43,3 +43,69 @@ person.name = 'Roky Das Shagotam'; // no error
 // person.name = 45; // error
 // person.country = 'Bangladesh' // error
 // we cannot add extra property in an object.
+// we can declare a specific typed variable 
+var newName;
+newName = "Roky Das";
+console.log(newName);
+// newName = 45; // error;
+var numberArray = [];
+numberArray.push(7);
+// numberArray.push('rokydas') // error
+// a variable declaration which can carry
+// number and string both type of value
+var numberOrString;
+numberOrString = 22;
+numberOrString = "Roky Das";
+// numberOrString = true; error
+var numberOrStringArray = [];
+numberOrStringArray.push('rokydas');
+numberOrStringArray.push(22);
+// numberOrStringArray.push(true); error
+console.log(numberOrStringArray);
+// object declaration
+var myObject;
+myObject = {
+    name: 'Roky Das',
+    age: 22,
+    cgpa: 3,
+};
+// myObject.isSingle = true; error
+// myObject.name = 'Roky Das Shagotam', error
+// we can add various type of property in this object.
+// Now we can specific the property type also in an object
+var myBiodata;
+myBiodata = {
+    name: 'Roky Das',
+    age: 22,
+    cgpa: 3,
+    isSingle: true,
+};
+var a; // we can assign any value to this variable. 
+// but it's not the ts's power.
+// any type of array
+var anyArray = [];
+anyArray.push("Bangladesh");
+anyArray.push(22);
+anyArray.push(true);
+// any type of object
+var country;
+country = {
+    name: 666,
+    area: 'It is area',
+    isSmall: 'True'
+};
+// Any type is not the good decision. 
+// If we use it, TS cannot give us extra facilities.
+//Function 
+// let myFunc: Function;
+// c?:string // optional parameter
+// c:string = 'Shagotam' // default parameter
+var myFunc = function (a, b, c) {
+    if (c === void 0) { c = 'Shagotam'; }
+    return "Hello " + a + " " + b + " " + c;
+};
+console.log(myFunc('Roky', 'Das'));
+// Difference between void and undefined
+// JS auto return undefined and TS return void
+// void means nothing and undefined means a variable
+// which is not defined. 
--- src/script.ts
+++ src/script.ts
@@ -57,3 +57,94 @@ person.name = 'Roky Das Shagotam' // no error
 // person.name = 45; // error
 // person.country = 'Bangladesh' // error
 // we cannot add extra property in an object.
+
+// we can declare a specific typed variable 
+let newName: string;
+newName = "Roky Das";
+console.log(newName);
+// newName = 45; // error;
+
+let numberArray: number[] = [];
+numberArray.push(7);
+// numberArray.push('rokydas') // error
+
+// a variable declaration which can carry
+// number and string both type of value
+let numberOrString: string | number;
+numberOrString = 22;
+numberOrString = "Roky Das";
+// numberOrString = true; error
+
+let numberOrStringArray: (string | number)[] = [];
+numberOrStringArray.push('rokydas');
+numberOrStringArray.push(22);
+// numberOrStringArray.push(true); error
+console.log(numberOrStringArray);
+
+// object declaration
+let myObject: object;
+myObject = {
+    name: 'Roky Das',
+    age: 22,
+    cgpa: 3,
+}
+// myObject.isSingle = true; error
+// myObject.name = 'Roky Das Shagotam', error
+// we can add various type of property in this object.
+
+// Now we can specific the property type also in an object
+let myBiodata: {
+    name: string,
+    age: number,
+    cgpa: number,
+    isSingle: boolean
+}
+
+myBiodata = {
+    name: 'Roky Das',
+    age: 22,
+    cgpa: 3,
+    isSingle: true,
+    // isWebDev: true // error
+}
+
+let a:any // we can assign any value to this variable. 
+// but it's not the ts's power.
+
+// any type of array
+let anyArray: any[] = [];
+anyArray.push("Bangladesh");
+anyArray.push(22);
+anyArray.push(true);
+
+// any type of object
+let country: {
+    name: any,
+    area: any,
+    isSmall: any
+}
+
+country = {
+    name: 666,
+    area: 'It is area',
+    isSmall: 'True'
+}
+
+// Any type is not the good decision. 
+// If we use it, TS cannot give us extra facilities.
+
+//Function 
+// let myFunc: Function;
+// c?:string // optional parameter
+// c:string = 'Shagotam' // default parameter
+
+const myFunc = (a: string, b: string, c:string = 'Shagotam') => {
+    return `Hello ${a} ${b} ${c}`;
+}
+
+console.log(myFunc('Roky', 'Das'));
+
+// Difference between void and undefined
+// JS auto return undefined and TS return void
+// void means nothing and undefined means a variable
+// which is not defined. 
